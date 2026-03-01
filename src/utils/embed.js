245499bbaf66b89dca4e0d764e6f7318dbea6581
sa_malu <68854747+sa-malu@users.noreply.github.com>
2026-03-01 13:29:33 -0300

    Add createEmbed function for Discord embeds

--- src/utils/embed.js
+++ src/utils/embed.js
@@ -1,1 +1,12 @@
+const { EmbedBuilder } = require("discord.js");
+const config = require("../config");
 
+function createEmbed(title, description, color = config.color) {
+  return new EmbedBuilder()
+    .setColor(color)
+    .setTitle(title)
+    .setDescription(description)
+    .setTimestamp();
+}
+
+module.exports = { createEmbed };
